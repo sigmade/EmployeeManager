7cf209e6a21abd7b1a62b43ddeeeff6b218b3cca
sigmade <sychyov1991@mail.ru>
2021-03-30 22:36:16 +0600

    chore(AccountController) Убрал лишние методы

--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -23,51 +23,6 @@ namespace WebAPI.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Register(RegisterDto registerDto)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == registerDto.Email);
-                if (user == null)
-                {
-                    // добавляем пользователя в бд
-                    user = new User { Email = registerDto.Email, Password = registerDto.Password };
-                    Role userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
-                    if (userRole != null)
-                        user.Role = userRole;
-
-                    _context.Users.Add(user);
-                    await _context.SaveChangesAsync();
-
-                    await Authenticate(user); // аутентификация
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
-            }
-            return NoContent();
-        }
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Login(LoginDto loginDto)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        User user = await _context.Users
-        //            .Include(u => u.Role)
-        //            .FirstOrDefaultAsync(u => u.Email == loginDto.Email && u.Password == loginDto.Password);
-        //        if (user != null)
-        //        {
-        //            await Authenticate(user); // аутентификация
-
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        ModelState.AddModelError("", "Некорректные логин и(или) пароль");
-        //    }
-        //    return NoContent();
-        //}
 
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
@@ -83,27 +38,6 @@ namespace WebAPI.Controllers
                 }
             }
             return Forbid();
-        }
-
-        private async Task Authenticate(User user)
-        {
-            // создаем один claim
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
-                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
-            };
-            // создаем объект ClaimsIdentity
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
-                ClaimsIdentity.DefaultRoleClaimType);
-            // установка аутентификационных куки
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
         }
     }
 }
